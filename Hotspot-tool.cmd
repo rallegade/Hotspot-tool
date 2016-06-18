a074f73064627f5785c097cbd506ae3fe3404e62
Rasmus Gade <ralle.gade@gmail.com>
2016-06-18 16:51:52 +0200

    Added code
    + option to get info about the hotspot
    + option of list of connected devices (Kudos to JamesCullum, kanged from his https://github.com/JamesCullum/Windows-Hotspot)

--- Hotspot-tool.cmd
+++ Hotspot-tool.cmd
@@ -55,7 +55,11 @@ ECHO [3] Turn off hotspot
 ECHO --------------------------------------------------------------------------------
 ECHO [4] Configure networkadapter
 ECHO --------------------------------------------------------------------------------
-ECHO [5] Close this script
+ECHO [5] Hotspot info
+ECHO --------------------------------------------------------------------------------
+ECHO [6] Connected devices
+ECHO --------------------------------------------------------------------------------
+ECHO [7] Close this script
 ECHO --------------------------------------------------------------------------------
 set /p choice=
 
@@ -65,12 +69,14 @@ if %choice%==1 GOTO Hotspotconfig
 if %choice%==2 GOTO Starthotspot
 if %choice%==3 GOTO Stophotspot
 if %choice%==4 GOTO Netconfig
-if %choice%==5 GOTO Closeprogram
+if %choice%==5 GOTO Hotspotinfo
+if %choice%==6 GOTO Connecteddevices
+if %choice%==7 GOTO Closeprogram
 ECHO Wrong input, please just enter a number between 1-5!.
 pause
 GOTO start
 
-rem  --> hotspot configuration code
+rem  --> option 1: hotspot configuration code
 :Hotspotconfig
 netsh wlan stop hostednetwork
 netsh wlan set hostednetwork mode=allow
@@ -85,23 +91,23 @@ cls
 netsh wlan set hostednetwork ssid=%name%
 netsh wlan set hostednetwork key=%key%
 netsh wlan show hostednetwork setting=security
-pause
 GOTO start
 
-rem  --> The start hotspot code
+rem  --> option 2: The start hotspot code
 :Starthotspot
 ECHO Starting hotspot
 netsh wlan start hostednetwork
 pause
 GOTO start
 
-rem  --> The stop hotspot code
+rem  --> option 3: The stop hotspot code
 :Stophotspot
 ECHO Stopping hotspot
 netsh wlan stop hostednetwork
 pause
 GOTO start
 
+rem  --> option 4: network adaptor settings
 rem  --> Temporary fix for first time setting the windows hostednetwork up. This is only valid until i get something done in powershell
 :Netconfig
 cls
@@ -130,6 +136,49 @@ ECHO ---------------------------------------------------------------------------
 pause
 GOTO start
 
-rem  --> closes the program
+rem  --> option 5: Shows information about the hotspot
+:Hotspotinfo
+cls
+netsh wlan show hostednetwork
+netsh wlan show hostednetwork setting=security
+pause
+GOTO start
+
+:Connecteddevices
+rem  --> option 6: Shows a list of connected devices
+@echo off 
+set hasClients=0
+arp -a | findstr /r "192\.168\.[0-9]*\.[2-9][^0-9] 192\.168\.[0-9]*\.[0-9][0-9][^0-9] 192\.168\.[0-9]*\.[0-1][0-9][0-9]" >test.tmp
+arp -a | findstr /r "192\.168\.[0-9]*\.2[0-46-9][0-9] 192\.168\.[0-9]*\.25[0-4]" >>test.tmp
+for /F "tokens=1,2,3" %%i in (test.tmp) do call :process %%i %%j %%k
+del test.tmp
+echo Connected Clients
+echo ------------------
+if %hasClients%==0 echo No device is currently connected to your hotspot
+if %hasClients%==1 (
+	type result.tmp
+	del result.tmp
+)
+echo ------------------
+pause
+GOTO start
+
+:process
+set VAR1=%1
+ping -a %VAR1% -n 1 | findstr Pinging > loop1.tmp
+for /F "tokens=1,2,3" %%i in (loop1.tmp) do call :process2 %%i %%j %%k
+del loop1.tmp
+goto :EOF
+
+:process2 
+SET VAR2=%2
+SET VAR3=%3
+set hasClients=1
+echo %VAR2% %VAR3% >>result.tmp
+goto :EOF 
+
+:EOF
+
+rem  --> option 7: closes the program
 :Closeprogram
 GOTO exit
